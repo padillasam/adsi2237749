d3befd196ab232ae0bffd4c848c336a35f47eb3e
padillasam <panarick@gmail.com>
2022-06-02 11:20:06 -0500

    http + archivos

--- http/http1.js
+++ http/http1.js
@@ -17,12 +17,30 @@
 // }).listen(3000);
 // console.log('escuchando http en pto 3000')
 
+// const http=require('http');
+// http.createServer(function(req,res){
+//     console.log('Nueva petición');
+//     console.log(req.url);
+//     res.writeHead(201,{'Content-Type':'text/html'})
+//     res.write('<h3>Hola estoy usando http de node</h3>')
+//     res.end();
+// }).listen(3000);
+// console.log('escuchando http en pto 3000')
+
 const http=require('http');
+const fs=require('fs');
 http.createServer(function(req,res){
-    console.log('Nueva petición');
-    console.log(req.url);
-    res.writeHead(201,{'Content-Type':'text/html'})
-    res.write('<h3>Hola estoy usando http de node</h3>')
+    res.setHeader('Content-Type','text/html');
+//fs.readFile(__dirname+'/view/index.html',(err,data)=>{
+    fs.readFile(__dirname+'/index.html',(err,data)=>{
+    if (err) {
+        console.log(err);
+        res.end();
+    } else {
+        res.write(data);
+        res.end();
+    }
     res.end();
+})
 }).listen(3000);
 console.log('escuchando http en pto 3000')
