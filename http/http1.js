852836bcdb1e14bdf7d481a2fb63860deb045f7d
padillasam <panarick@gmail.com>
2022-06-08 07:33:15 -0500

    hasta http

--- http/http1.js
+++ http/http1.js
@@ -27,10 +27,31 @@
 // }).listen(3000);
 // console.log('escuchando http en pto 3000')
 
+//PRACTICA 2
+// const http=require('http');
+// const fs=require('fs');
+// http.createServer(function(req,res){
+//     res.setHeader('Content-Type','text/html');
+// //fs.readFile(__dirname+'/view/index.html',(err,data)=>{
+//     fs.readFile(__dirname+'/index.html',(err,data)=>{
+//     if (err) {
+//         console.log(err);
+//         res.end();
+//     } else {
+//         res.write(data);
+//         res.end();
+//     }
+//     res.end();
+// })
+// }).listen(3000);
+// console.log('escuchando http en pto 3000')
+
+
 const http=require('http');
 const fs=require('fs');
 http.createServer(function(req,res){
     res.setHeader('Content-Type','text/html');
+    
 //fs.readFile(__dirname+'/view/index.html',(err,data)=>{
     fs.readFile(__dirname+'/index.html',(err,data)=>{
     if (err) {
--- objetos/destructuracion.js
+++ objetos/destructuracion.js
@@ -3,10 +3,17 @@
 const literal={
     nombre:'juan',
     apellido:'rodriguez',
+    funcion1:()=>{
+        console.log('soy la función 1')
+    },
+    funcion2:function(){console.log('soy la función2 ')}
 }
 console.log(literal)
 
 // const {nombre,apellido}=literal;
 // console.log(`solo el nombre es: ${nombre}`)
-let {apellido}=literal;
-console.log(`solo el nombre es: ${apellido}`)
+let {funcion1}=literal;
+let {funcion2}=literal;
+//console.log(`solo el nombre es: ${apellido}`)
+funcion1();
+funcion2();
